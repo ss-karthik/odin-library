7a4665d24985172dc5471eb8d7c0326c6284ed7c
ss-karthik <maniac.sskarthik@gmail.com>
2024-02-15 22:03:38 +0530

    Add display function to display the cards

--- script.js
+++ script.js
@@ -1,9 +1,54 @@
+const myLibrary = [];
+
 function Book(title, author, pageCount, status) {
+	this.id = myLibrary.length;
     this.title = title;
     this.author = author;
     this.pageCount = pageCount;
     this.status = status;
-    this.info = function() {
-        return title + ", by " + author + ", " + pageCount + " pages, " + status; 
+    this.changeStatus = function() {
+        if (status == 1) {
+        	status = 0;
+        } else {
+        	status = 1;
+        }
     }
+}
+
+
+function displayBooks() {
+	let cards = document.querySelector('.card-container');
+	cards.innerHTML = "";
+	myLibrary.forEach((item) => {
+		let container = document.createElement('div');
+		container.classList.add('card');
+		container.id = item.id;
+		let title = document.createElement('h1');
+		let author = document.createElement('h2');
+		let pcount = document.createElement('h2');
+		let status = document.createElement('h2');
+		let changeStatus = document.createElement('button');
+		
+		title.innerHTML = item.title;
+		author.innerHTML = 'By: ' + item.author;
+		pcount.innerHTML = item.pageCount + ' pages';
+		changeStatus.innerHTML = 'Change Read Status';
+		changeStatus.addEventListener('click', item.changeStatus());
+		if (status == 1) {
+			status.innerHTML = 'Book has been read';
+		} else {
+			status.innerHTML = 'Book has not been read';
+		}
+		
+		container.appendChild(title);
+		container.appendChild(author);
+		container.appendChild(pcount);
+		container.appendChild(status);
+		container.appendChild(changeStatus);
+		cards.appendChild(container);
+	});
+}
+
+function addBook() {
+	
 }
