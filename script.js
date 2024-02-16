f7185273d0b44ef1419bb4d6dd88eef0d40e7fb9
ss-karthik <maniac.sskarthik@gmail.com>
2024-02-16 11:56:56 +0530

    Add all site functionality (create, modify and delete books)

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const myLibrary = [];
 
 function Book(title, author, pageCount, status) {
@@ -7,10 +8,10 @@ function Book(title, author, pageCount, status) {
     this.pageCount = pageCount;
     this.status = status;
     this.changeStatus = function() {
-        if (status == 1) {
-        	status = 0;
+        if (this.status == 1) {
+        	this.status = 0;
         } else {
-        	status = 1;
+        	this.status = 1;
         }
     }
 }
@@ -28,13 +29,21 @@ function displayBooks() {
 		let pcount = document.createElement('h2');
 		let status = document.createElement('h2');
 		let changeStatus = document.createElement('button');
-		
+		let delBook = document.createElement('button');	
 		title.innerHTML = item.title;
 		author.innerHTML = 'By: ' + item.author;
 		pcount.innerHTML = item.pageCount + ' pages';
 		changeStatus.innerHTML = 'Change Read Status';
-		changeStatus.addEventListener('click', item.changeStatus());
-		if (status == 1) {
+		delBook.innerHTML = 'Delete Book';
+		changeStatus.addEventListener('click', function() {
+			item.changeStatus();
+			displayBooks();
+		});
+		delBook.addEventListener('click', function(e) {
+			deleteBook(e);
+			displayBooks();
+		})
+		if (item.status == 1) {
 			status.innerHTML = 'Book has been read';
 		} else {
 			status.innerHTML = 'Book has not been read';
@@ -45,10 +54,49 @@ function displayBooks() {
 		container.appendChild(pcount);
 		container.appendChild(status);
 		container.appendChild(changeStatus);
+		container.appendChild(delBook);
 		cards.appendChild(container);
 	});
 }
 
+const addButton = document.querySelector('.new-book');
+const submitButton = document.querySelector('.submit-book');
+const dialog = document.querySelector("dialog");
+
+const titleBox = document.getElementById('title');
+const authorBox = document.getElementById('author'); 
+const pageBox = document.getElementById('pages');
+const readBox = document.getElementById('readStatus');
+
+addButton.addEventListener('click', function() {
+	dialog.showModal();
+});
+
+submitButton.addEventListener('click', function(e) {
+	e.preventDefault();
+	addBook();
+});
+
 function addBook() {
+	let status;
+	if (readBox.checked == true) {
+		status = 1;
+	} else {
+		status = 0;
+	}
 	
+	let newBook = new Book(titleBox.value, authorBox.value, pageBox.value, status);		
+	myLibrary.push(newBook);
+	dialog.close();
+	displayBooks();
+
+	titleBox.value = '';
+	authorBox.value = '';
+	pageBox.value = '';
+	readBox.checked = false;
 }
+
+function deleteBook(e) {
+	let id = e.target.parentElement.id;
+	myLibrary.splice(id, 1);
+}	
